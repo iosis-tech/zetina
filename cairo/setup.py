ad0475018fb12b73784ca395055a35a12d4b7c9e
Bartosz Nowak <bartekn337@gmail.com>
2024-04-14 16:18:05 +0200

    sketch

---
+++ cairo/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="sharp_p2p_bootloader",
+    version="0.1",
+    description="sharp_p2p bootloader",
+    url="#",
+    author="Okm165",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
